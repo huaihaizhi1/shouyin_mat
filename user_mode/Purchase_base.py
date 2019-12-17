522bb278a3d96f2e539d07f72c0a21d8463f5e69
huaihaizhi1 <274729692@qq.com>
2019-12-17 18:49:19 +0800

    采购管理

--- user_mode/Purchase_base.py
+++ user_mode/Purchase_base.py
@@ -21,10 +21,20 @@ def purchase_goods(request_body,path):
     remarks=request_body.get('remarks',None)
     price_status=request_body.get('price_status',None)
     goods=request_body.get('goods',None)
-    print(request_body)
     date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     if path=='/select_purchase':
         list1=['code_id','purchase_date','suppiler_name','price_status','goods','purchas_price','user_name']
-        select_sql="select code_id,purchase_no,purchase_date,suppiler_name,purchas_price,price_status,user_name from t_purchase_table"
-        tem_sql=' where '
+        select_sql="select code_id,purchase_no,purchase_date,suppiler_name,purchas_price,price_status,user_name from t_purchase_table where shop_id='{0}'".format(id)
+        list2=[]
+        for i in request_body:
+            print(i, request_body.get(i))
+            if i in list1:
+                list2.append(i)
+        print(list2)
+        tmp_sql1=''
+        for i  in range(0,len(list2)):
+            tmp_sql=" and {0}='{1}' ".format(list2[i], request_body.get(list2[i]))
+            tmp_sql1=tmp_sql1+tmp_sql
+        print(tmp_sql1)
+        select_sql=select_sql+tmp_sql1
         print(select_sql)
